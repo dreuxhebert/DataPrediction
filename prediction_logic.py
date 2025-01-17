fdaa4486fe3472991c0613518795bd8797c4be69
dreuxhebert <143906733+dreuxhebert@users.noreply.github.com>
2025-01-17 03:46:30 -0500

    Update prediction_logic.py

--- prediction_logic.py
+++ prediction_logic.py
@@ -30,7 +30,7 @@ def run_predictions(filepath, algorithm):
         
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Dynamically import and execute the selected algorithm
+        # import and execute the selected algorithm
         module_name = f"algorithms.{algorithm}"
         try:
             module = importlib.import_module(module_name)
